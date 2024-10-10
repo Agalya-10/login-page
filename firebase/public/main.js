e06afcaf3b7a1cc5c9be5f82a9d01618c90a71a9
Agalya-10 <aagalya360@gmail.com>
2024-10-10 16:44:51 +0530

    Register form

--- firebase/public/main.js
+++ firebase/public/main.js
@@ -73,11 +73,13 @@ function MyButton(event) {
     let userName = document.getElementById('userName').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+    let cpassword = document.getElementById('cpassword').value;
   
     // Get error message elements
     let nameError = document.getElementById('nameError');
     let emailError = document.getElementById('emailError');
     let passwordError = document.getElementById('passwordError');
+    let cpasswordError = document.getElementById('cpasswordError');
   
     let valid = true;
   
@@ -85,6 +87,8 @@ function MyButton(event) {
     if (userName.trim() === "") {
         nameError.textContent = "Username is required*";
         nameError.style.color = "red";
+        nameError.style.fontSize="13px";
+        nameError.style.paddingLeft="15px";
         valid = false;
     } else {
         nameError.textContent = '';
@@ -93,6 +97,9 @@ function MyButton(event) {
     if (email.trim() === "") {
         emailError.textContent = "Email is required*";
         emailError.style.color = "red";
+        emailError.style.fontSize="13px";
+        emailError.style.paddingLeft="15px";
+
         valid = false;
     } else {
         emailError.textContent = '';
@@ -101,9 +108,24 @@ function MyButton(event) {
     if (password.trim() === "") {
         passwordError.textContent = "Password is required*";
         passwordError.style.color = "red";
+        passwordError.style.fontSize="13px";
+        passwordError.style.paddingLeft="15px";
+
         valid = false;
     } else {
         passwordError.textContent = '';
+    }
+
+    if (cpassword.trim() === "") {
+        cpasswordError.textContent = "Confirm Password is required*";
+        cpasswordError.style.color = "red";
+        cpasswordError.style.fontSize="13px";
+        cpasswordError.style.paddingLeft="15px";
+        
+
+        valid = false;
+    } else {
+        cpasswordError.textContent = '';
     }
   
     //print table
@@ -116,22 +138,24 @@ function MyButton(event) {
         let cell2 = newRow.insertCell(1);
         let cell3 = newRow.insertCell(2);
         let cell4 = newRow.insertCell(3);
+        let cell5 = newRow.insertCell(4);
   
         cell1.textContent = userName;
         cell2.textContent = email;
         cell3.textContent = password;
+        cell4.textContent = cpassword;
   
         // edit and delete
-        cell4.innerHTML = `
+        cell5.innerHTML = `
           <button class="btn bg-success edit-btn">Edit</button>
           <button class="btn bg-danger delete-btn">Delete</button>
         `;
 
-        cell4.querySelector('.edit-btn').addEventListener('click', function() {
+        cell5.querySelector('.edit-btn').addEventListener('click', function() {
             EditRow(newRow);
         });
   
-        cell4.querySelector('.delete-btn').addEventListener('click', function() {
+        cell5.querySelector('.delete-btn').addEventListener('click', function() {
             DeleteRow(newRow);
         });
   
@@ -139,17 +163,34 @@ function MyButton(event) {
         document.getElementById('form').reset();
     }
   }
-  
+ 
+  function togglePassword(eyeIcon) {
+    var passwordField = eyeIcon.previousElementSibling; 
+   
+    if (passwordField.type === "password") {
+        passwordField.type = "text"; 
+        eyeIcon.textContent = "🔓"; 
+    } else {
+        passwordField.type = "password"; 
+        eyeIcon.textContent = "🔒"; 
+    }
+}
+
+
   //edit row
   function EditRow(row) {
     let userName = row.cells[0].textContent;
     let email = row.cells[1].textContent;
     let password = row.cells[2].textContent;
+    let cpassword = row.cells[3].textContent;
+  
   
 
     document.getElementById('userName').value = userName;
     document.getElementById('email').value = email;
     document.getElementById('password').value = password;
+    document.getElementById('cpassword').value = cpassword;
+
 //   remove row
     row.remove();
   }
@@ -158,4 +199,3 @@ function MyButton(event) {
   function DeleteRow(row) {
     row.remove(); // Delete  row
   }
-  
